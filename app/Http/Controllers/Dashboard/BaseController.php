3a4fb354405c491c458d47a6a89226ba043f98fe
YuldashovAhror <ahroryuldashov123@gmail.com>
2023-06-06 18:30:07 +0500

    first commit

---
+++ app/Http/Controllers/Dashboard/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+
+use App\Traits\DestroyTrait;
+use App\Traits\FileTrait;
+use App\Traits\StoreTrait;
+use App\Traits\UpdateTrait;
+
+class BaseController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
